04961fea12f780f287f3a28ab1ffceb94906a7a6
João André Ribeiro Santos Maia <ist1102428@i5-065.tagus.ist.utl.pt>
2022-03-15 14:37:35 +0000

    ex07

---
+++ lab02/ex07.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(void){
+    float max,min,atual=0;
+    int total=0;
+    scanf("%d", &total);
+    int i=0;
+    while(i<total){
+        if(i==0){
+            scanf("%f", &atual);
+            max = atual;
+            min = atual;
+            i++;
+        }
+        else{
+            i++;
+            scanf("%f", &atual);
+            if(atual > max)
+                max = atual;
+            
+            else if(atual < min)
+                min = atual;
+        }
+        printf("i:%d\n",i);
+    }
+    printf("min: %f, max: %f\n", min, max);
+    return 0;
+}
