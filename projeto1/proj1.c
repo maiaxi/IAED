19d3367dc2834a7ba9911e893034a8710aef0ad1
maiaxi <jandremaia2003@gmail.com>
2022-03-17 15:02:03 +0000

    Proj1.c

---
+++ projeto1/proj1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+int main(void){
+    char command;
+    scanf("%s", &command);
+    if (command=="a"){
+        printf("%s", command);
+    }
+    return 0;
+}
