61424ac83921ccb97da1252a0b7e727956df8966
maiaxi <jandremaia2003@gmail.com>
2022-03-17 19:10:44 +0000

    Proj1.c

--- projeto1/proj1.c
+++ projeto1/proj1.c
@@ -3,10 +3,20 @@
 #include<ctype.h>
 #include<string.h>
 int main(void){
-    char command;
-    scanf("%s", &command);
-    if (command=="a"){
-        printf("%s", command);
+    char command[1];
+    int res;
+    int i;
+    scanf("%s", command);
+        for (i=0; i<1; i++){
+            res = command[i];
+        }
+    printf("%c \n", res);
+    if(res == 'q'){
+        printf("Acabou");
+        exit(0);
+    }
+    else{
+        printf("vais fazer outra coisa");
     }
     return 0;
 }
