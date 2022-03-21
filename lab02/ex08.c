6ff871935a92ac96e6678074ad436de09730aecc
maiaxi <jandremaia2003@gmail.com>
2022-03-21 23:46:00 +0000

    ex08.c

---
+++ lab02/ex08.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void){
+    int d = 0;
+    int n = 0;
+    int i = 0;
+    scanf("%d", &n);
+    if(n == 1){
+        printf("1");
+        return 0;
+    }
+    for (i=0; n % )
