0123e38b6915509d6eddae890014a2ff617dde0d
maiaxi <jandremaia2003@gmail.com>
2022-03-22 00:51:18 +0000

    ex01.c

--- lab03/ex01.c
+++ lab03/ex01.c
@@ -7,11 +7,12 @@ int main(void){
     int n;
     scanf("%d", &n);
     quadrado(n);
+    return 0;
 }
 void quadrado(int n){
     int colunas = 0 , linhas = 1;
     while (colunas < n+linhas -1){
-        printf("%d ", ++colunas);
+        printf("%d\t", ++colunas);
     
         if (colunas == n+linhas -1){
             colunas = linhas;
