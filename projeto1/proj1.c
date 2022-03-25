e40b4b6839c14448e984d0a69b7879b93783e281
maiaxi <jandremaia2003@gmail.com>
2022-03-25 14:51:33 +0000

    proj1

--- projeto1/proj1.c
+++ projeto1/proj1.c
@@ -2,37 +2,36 @@
 #include<stdlib.h>
 #include<ctype.h>
 #include<string.h>
+
 #define DIM 88
+#define AIRPORTS_MAX 40
 
+typedef struct airport{
+            char ID[3];
+            char country[30];
+            char city[50];
+            int incflights;
+            int outflights;
+} airport;
 
-/*struct airport {
-    char ID[3];
-    char country[30];
-    char city[50];
-    int incflights;
-    int outflights;
-}*/
+airport list_airports[AIRPORTS_MAX];
+
 void newapt(char command){
-    int apt[DIM];
-    int i = 2;
-    for (i; i<DIM; i++){
-        apt += command[i];
-    }
-    printf("%ls", apt);
- }
+	int i = 2, actual;
+	actual = command[i];
+	for(i; actual != ' '; i++){
+		printf("%c", command[i]);
+	}
+}
 
 int main(void){
     char command[DIM];
-    char apt[DIM];
-    int res;
-    int i;
-    fgets(command, DIM, stdin);
-    res = command[0];
-    switch(res){
+    int i, res;
+    scanf("%[^\n]%*c", command);
+    switch(command[0]){
         case('q'):
-            return 0;
+            exit(0);
         case('a'):
-            apt = newapt(command);
-            return 0;
+            newapt(command);
     }
 }
