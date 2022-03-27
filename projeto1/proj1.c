96403ddfcbb9d3c8c5ac099ebb8ab071564d36de
maiaxi <jandremaia2003@gmail.com>
2022-03-27 17:41:49 +0100

    proj1.c

--- projeto1/proj1.c
+++ projeto1/proj1.c
@@ -6,32 +6,89 @@
 #define DIM 88
 #define AIRPORTS_MAX 40
 
-typedef struct airport{
-            char ID[3];
-            char country[30];
-            char city[50];
-            int incflights;
-            int outflights;
-} airport;
+struct airport{
+    char id[3];
+    char country[30];
+    char city[50];
+    int incflights;
+    int outflights;
+};
 
-airport list_airports[AIRPORTS_MAX];
+struct airport list_airports[AIRPORTS_MAX];
 
-void newapt(char command){
-	int i = 2, actual;
-	actual = command[i];
-	for(i; actual != ' '; i++){
-		printf("%c", command[i]);
-	}
+int main(void);
+
+char* getid(char *command,char *id){
+    int i = 2, e = 0;
+    int idsize;
+    while (i < 5 ){
+        id[e] = command[i];
+        e++;
+        i++;
+    }
+    idsize = strlen(id);
+    if (idsize != 3){
+        printf("invalid airport ID");
+        main();
+    }
+    for (i=0; i<idsize; i++){
+        if (id[i]<65 || id[i] > 90){
+            printf("invalid airport ID");
+            main();
+        }
+    }
+    return id;
+}
+
+void getcountry(char* command, char* country){
+    int i = 6, e = 0;
+    while (command[i]!= ' '){
+        country[e] = command[i];
+        e++;
+        i++;
+    }
+}
+
+void getcity(char* command, char* city){
+    int i = 6, e = 0;
+    int size;
+    size = strlen(command);
+    while (command[i]!= ' '){
+        e++;
+        i++;
+    }
+    if(i+2 != size){
+        i++;
+        e = 0;
+    }
+    while (i < size){
+        city[e] = command [i];
+        i++;
+        e++;
+    }
+}
+
+
+void newapt(char * command){
+    struct airport apt;
+    char id[3];
+    char country[30];
+    char city[50];
+    getid(command, id);
+    getcountry(command, country);
+    getcity(command, city);
+    strcpy(apt.id , id);
+    printf("airport %s", apt.id);
 }
 
 int main(void){
     char command[DIM];
-    int i, res;
     scanf("%[^\n]%*c", command);
-    switch(command[0]){
-        case('q'):
+    switch (command[0]){
+        case ('q'):
             exit(0);
-        case('a'):
+        case ('a'):
             newapt(command);
     }
+    return 0;
 }
